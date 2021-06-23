ea3b0caaacdc1a9c6d1d40501a7a294de6e924de
Benjamin Efenudu <benjaminefenudu@gmail.com>
2021-06-23 12:01:18 +0100

    Serving a HTML Page From a File

--- createServer.js
+++ createServer.js
@@ -1,12 +1,22 @@
 const http = require('http');
+const fs = require('fs').promises;
 
 const host = 'localhost';
 const port = 8080;
 
 const requestListener = function (req, res) {
-  res.writeHead(200);
-  res.end('Hello, World!');
-}
+    fs.readFile(__dirname + '/index.html')
+        .then (contents => {    
+            res.setHeader("Content-Type", "text/html")
+            res.writeHead(200);
+            res.end(contents);
+        })
+        .catch( err => {
+            res.writeHead(500);
+            res.end(err);
+            return;
+        })
+};
 
 const server = http.createServer(requestListener);
 server.listen(8080, host, () => {
